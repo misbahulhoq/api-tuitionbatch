e7aca6a69b2c7ce5debb4187ba1bfa145f278143
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-09-05 19:24:10 +0600

    lock file -changed

--- dist/config/env.config.js
+++ dist/config/env.config.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.envVars = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const requiredEnvVars = ["MONGO_URL", "AUTH_GOOGLE_SECRET", "AUTH_GOOGLE_ID"];
+const requiredEnvVars = ["MONGO_URL"];
 requiredEnvVars.map((envVar) => {
     if (!process.env[envVar]) {
         throw new Error(`Missing environment variable, ${envVar}`);
@@ -14,6 +14,4 @@ requiredEnvVars.map((envVar) => {
 });
 exports.envVars = {
     MONGO_URL: process.env.MONGO_URL,
-    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
-    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
 };
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const requiredEnvVars = ["MONGO_URL", "AUTH_GOOGLE_SECRET", "AUTH_GOOGLE_ID"];
+const requiredEnvVars = ["MONGO_URL"];
 
 requiredEnvVars.map((envVar) => {
   if (!process.env[envVar]) {
@@ -11,6 +11,4 @@ requiredEnvVars.map((envVar) => {
 
 export const envVars = {
   MONGO_URL: process.env.MONGO_URL,
-  AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
-  AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
 };
